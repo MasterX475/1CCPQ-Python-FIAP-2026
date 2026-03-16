cec7670607a1f51885e84889517c957deb3be6f8
gabrielsilveira <gabrielus1000000@gmail.com>
2026-03-16 20:45:41 -0300

    Aula 2 sobre variáveis.

---
+++ Aula-02-Variaveis-e-Operadores/aula-02-desafio.py
@@ -0,0 +1,19 @@
+#EX1
+
+nome = input("Qual seu nome? ")
+print(f"Bem vindo {nome}!")
+
+#EX2
+
+dia = input("Qual seu dia de nascimento? ")
+mes = input("Qual seu mês de nascimento? ")
+ano = input("Qual seu ano de nascimento? ")
+
+print(f"Você nasceu em {dia}/{mes}/{ano}")
+
+#EX3
+
+num1 = float(input("Dê me um número: "))
+num2 = float(input("Dê me outro número: "))
+
+print(f"A soma entre eles é {num1 + num2}")
---
+++ Aula-02-Variaveis-e-Operadores/aula-02a-variaveis.py
@@ -0,0 +1,23 @@
+print("Olá mundo")
+
+print(7 + 4)
+
+print("7" + "4")
+
+#Variáveis
+
+nome = "Gabriel"
+idade = 20
+peso = 70
+
+print("Olá", nome, idade, peso)
+
+print(f"Olá, {nome}, {idade}, {peso}")
+
+#Input
+
+nome2 = input("Qual seu nome: ")
+idade2 = input("Qual sua idade: ")
+peso2 = input("Qual seu peso: ")
+
+print(nome2, idade2, peso2)
