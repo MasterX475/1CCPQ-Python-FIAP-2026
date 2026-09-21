fae870ce15b9bbaf7b1f4f35eb4f7ca8f53f5f4c
Gabriel Silveira <gabrielus1000000@gmail.com>
2026-09-21 20:39:53 -0300

    aula 9

--- Aula-08-mini-crm/app.py
+++ Aula-08-mini-crm/app.py
@@ -24,15 +24,33 @@ def add_lead():
 
 def list_leads():
     leads = control.read_leads()
-    print(leads)
+    for i, lead in enumerate(leads):
+        print(f"{i} | {lead["name"]:<12} | {lead["email"]}")
 
     # Desafio - Formatar como tabela
+
+def search_leads():
+    query = input("Buscar por: ").strip().lower()
+    search_results = control.read_leads_search(query)
+
+    print(f"## | {"Nome":<12} | E-mail")
+    for i, lead in search_results:
+        print(f"{i:02d} | {lead["name"]:<12} | {lead["email"]}")
+
+def export_leads():
+    path_csv = control.export_csv()
+    if path_csv == None:
+        print("Não foi possível exportar para CSV")
+    else:
+        print(f"Exportado para {path_csv}")
 
 def main():
     while True:
         print("\nMini CRM de Leads")
         print("[1] Adicionar lead")
         print("[2] Listar lead")
+        print("[3] Buscar (nome/email)")
+        print("[4] Exportar para CSV")
         print("[0] Sair")
 
         opt = input("Escolha uma opção: ")
@@ -42,6 +60,11 @@ def main():
             add_lead()
         elif opt == "2":
             list_leads()
+        elif opt == "3":
+            search_leads()
+        elif opt == "4":
+            export_leads()
+
         elif opt == "0":
             print("Programa encerrado")
             break
--- Aula-08-mini-crm/control.py
+++ Aula-08-mini-crm/control.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-import json
+import json, csv
 
 DATA_DIR = Path(__file__).resolve().parent / "data"
 DATA_DIR.mkdir(exist_ok=True)
@@ -22,3 +22,34 @@ def create_lead(lead_dict):
     leads = read_leads()
     leads.append(lead_dict)
     DB_PATH.write_text(json.dumps(leads, ensure_ascii=False, indent=2), encoding="utf-8")
+
+#Buscar leads
+def read_leads_search(query):
+    #Busca Leads de acordo com query e rotorna uma lista de resultados
+    leads = read_leads()
+    results = []
+
+    for i, lead in enumerate(leads):
+        txt_lead = f"{lead["name"]} {lead["email"]}".lower()
+
+        if query.lower() in txt_lead:
+            results.append((i,lead))
+
+    return results
+
+def export_csv():
+    #Exporta tdoas as lead para CSV e rotorna o caminho do CSV
+    path_csv = DATA_DIR / "leads.csv"
+    leads = read_leads()
+
+    try:
+        with path_csv.open("w", newline="", encoding="utf-8") as file_csv:
+            writer = csv.DictWriter(file_csv, leads[0].keys())
+            writer.writeheader()
+            for row_dict in leads:
+                writer.writerow(row_dict)
+
+        return path_csv
+
+    except PermissionError:
+        return None
